e4b34557f52b3c8bd9ac6ba3f8a0011a556162c6
Faizer <99070117+faizer001@users.noreply.github.com>
2025-11-27 00:40:09 +0600

    Add files via upload

---
+++ BST.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+
+Node* insertNode(Node* root, int value) {
+    if (root == NULL)
+        return createNode(value);
+
+    if (value < root->data)
+        root->left = insertNode(root->left, value);
+    else
+        root->right = insertNode(root->right, value);
+
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+void preorder(Node* root) {
+    if (root != NULL) {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node* root) {
+    if (root != NULL) {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    int n, value;
+
+    cout << "Enter number of nodes to insert: ";
+    cin >> n;
+
+    cout << "Enter values:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> value;
+        root = insertNode(root, value);
+    }
+
+    cout << "\nInorder Traversal: ";
+    inorder(root);
+
+    cout << "\nPreorder Traversal: ";
+    preorder(root);
+
+    cout << "\nPostorder Traversal: ";
+    postorder(root);
+
+    cout << endl;
+    return 0;
+}
+
