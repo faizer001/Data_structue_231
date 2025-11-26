63fb115dd97eb238198f2a9993e1a56cfe5e2a49
Faizer <99070117+faizer001@users.noreply.github.com>
2025-11-27 00:27:43 +0600

    Add files via upload

---
+++ TowerOfHanoi.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char fromRod, char toRod, char auxRod) {
+    if (n == 1) {
+        cout << "Move disk 1 from " << fromRod << " to " << toRod << endl;
+        return;
+    }
+
+    towerOfHanoi(n - 1, fromRod, auxRod, toRod);
+
+
+    cout << "Move disk " << n << " from " << fromRod << " to " << toRod << endl;
+
+    towerOfHanoi(n - 1, auxRod, toRod, fromRod);
+}
+
+int main() {
+    int n;
+
+    cout << "Enter number of disks: ";
+    cin >> n;
+
+    cout << "\nSteps to solve Tower of Hanoi:\n";
+    towerOfHanoi(n, 'A', 'C', 'B');  // A = source, C = destination, B = auxiliary
+
+    return 0;
+}
+
